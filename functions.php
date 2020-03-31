7266a51e077d3fcbe033660fa6e0ae821ebc4d11
Vladimír Smitka <vladimir.smitka@lynt.cz>
2020-03-31 15:01:25 +0200

    upraven loader skriptů - odstraněn duplicitní FA

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php 
-	  
-     function naswp_kit_atomic_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  }
-           
-           
-     add_action( 'wp_enqueue_scripts', 'naswp_kit_atomic_enqueue_styles' ); 
+    function naswp_kit_atomic_script_css_loader() {
+ 	    //load parent styles
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+        
+        //unload font awesome from atomic blocks plugin
+        wp_dequeue_style('atomic-blocks-fontawesome');
+        wp_deregister_style('atomic-blocks-fontawesome');
+    }
+    
+    
+    
+    add_action( 'wp_enqueue_scripts', 'naswp_kit_atomic_script_css_loader' ); 
  ?>
