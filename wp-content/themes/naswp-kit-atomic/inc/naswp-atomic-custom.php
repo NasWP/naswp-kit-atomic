d71daebcc2f33876c2b5c2eb4ac7b4b3887f1b08
Vladimír Smitka <vladimir.smitka@lynt.cz>
2020-04-17 20:54:42 +0200

    resource hints pro google fonty

--- wp-content/themes/naswp-kit-atomic/inc/naswp-atomic-custom.php
+++ wp-content/themes/naswp-kit-atomic/inc/naswp-atomic-custom.php
@@ -1,9 +1,30 @@
 <?php
 /* Do tohoto souboru patří funkce pro vlastní úpravy šablony */
 
-
 //vlastní fonty
 function atomic_blocks_fonts_url()
 {
 	return 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700&display=swap&subset=latin-ext';
 }
+
+//úprava resource hints pro načítání google fontů
+function naswp_resource_hints($hints, $relation_type)
+{
+	if ($relation_type === 'preconnect') {
+		$hints[] = '//fonts.gstatic.com';
+		$hints[] = '//fonts.googleapis.com';
+
+		if ($relation_type === 'dns-prefetch') {
+			$urls = array('fonts.googleapis.com', 'fonts.gstatic.com');
+
+			foreach ($urls as $url) {
+				if (($key = array_search($url, $hints)) !== false) {
+					unset($hints[$key]);
+				}
+			}
+		}
+		return $hints;
+	}
+}
+
+add_filter('wp_resource_hints', 'naswp_resource_hints', 10, 2);
